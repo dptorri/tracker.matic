de8275074ebdc91fc495c3058d2f1c5cc1a990b7
Daniel Palacios <dptorri@gmail.com>
2017-11-17 10:19:26 +0100

    App.js added

---
+++ dist/worklog.bundle.js
@@ -0,0 +1,27 @@
+webpackJsonp([0],{
+
+/***/ 15:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _react = __webpack_require__(2);
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactDom = __webpack_require__(8);
+
+var _reactDom2 = _interopRequireDefault(_reactDom);
+
+var _App = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"./component/App.js\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+
+var _App2 = _interopRequireDefault(_App);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+_reactDom2.default.render(_react2.default.createElement(_App2.default, null), document.getElementById('react-app'));
+
+/***/ })
+
+},[15]);
---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class App extends React.Component{
+    render(){
+        return(
+
+//header
+
+//post_task
+            <div id="mainPage">
+                
+                <form action="">
+                    <input type="text" className="name"/><br />
+                    <input type="text" className="name"/>
+                </form>
+                
+            </div>
+//list_task
+
+//footer   
+
+
+        );
+    }
+}
